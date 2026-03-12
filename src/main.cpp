d7162306c42b61b500a09176205756ce84ea35b5
esaumuieh <markmlrave@gmail.com>
2026-03-13 02:10:43 +0530

    Add blinking LED code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-#define LED_PIN 2 // Most ESP32 dev boards have an internal blue LED on pin 2
+// Most ESP32 boards have a built-in blue LED on pin 2
+#define LED_PIN 2 
 
 void setup() {
-  Serial.begin(115200);
+  // Tell the ESP32 that pin 2 is an output
   pinMode(LED_PIN, OUTPUT);
-  Serial.println("System starting...");
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  Serial.println("LED is ON");
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  Serial.println("LED is OFF");
-  delay(1000);
+  digitalWrite(LED_PIN, HIGH);  // Turn the LED on
+  delay(1000);                  // Wait for 1 second (1000 milliseconds)
+  digitalWrite(LED_PIN, LOW);   // Turn the LED off
+  delay(1000);                  // Wait for 1 second
 }
